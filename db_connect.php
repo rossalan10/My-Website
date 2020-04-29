daf3de825caeddfbe953f5da616bc0ff9cb15ea4
rossalan10 <64547508+rossalan10@users.noreply.github.com>
2020-04-29 12:51:00 -0400

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // variables to connect to the database
-	$host = "127.0.0.1";
-	$username = "root";
-	$user_pass = "";
-	$database_in_use = "test";
+	$host = "localhost";
+	$username = "u939875983_root";
+	$user_pass = "12345";
+	$database_in_use = "u939875983_test";
 
 // create a database connection
 	$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
